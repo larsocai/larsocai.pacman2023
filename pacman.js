2aa61f31b9b5a505b0f9789f952a12c16e4186e1
Caitlyn Larson <124397489+larsocai@users.noreply.github.com>
2023-02-26 08:59:09 -0800

    Add files via upload

---
+++ pacman.js
@@ -0,0 +1,39 @@
+var pos = 0;
+let pageWidth = window.innerWidth;
+const pacArray = [
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
+];
+
+// this variable defines what direction should PacMan go into:
+// 0 = left to right
+// 1 = right to left (reverse)
+var direction = 0;
+
+// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+var focus = 0;
+
+
+function Run() {
+  let img = document.getElementById('PacMan');
+  let imgWidth = img.width;
+  focus = (focus + 1) % 2;
+  direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
+  img.src = pacArray[direction][focus];
+  if (direction) {
+    pos -= 20;
+    img.style.left = pos + 'px';
+  } else {
+    pos += 20;
+    img.style.left = pos + 'px';
+  }
+  setTimeout(Run, 150);
+}
+
+
+ 
+function checkPageBounds(direction, imgWidth, pos, pageWidth) {
+  if (direction == 0 && pos + imgWidth > pageWidth) direction = 1;
+  if (direction == 1 && pos < 0) direction = 0;
+  return direction;
+}
